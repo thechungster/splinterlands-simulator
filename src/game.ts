f23065a53ec37129900250fda538e89d84b379db
Tim <timmmmy400@gmail.com>
2022-06-23 21:27:24 -0500

    Resurrected armors are set back to max armor

--- src/game.ts
+++ src/game.ts
@@ -735,7 +735,7 @@ export class Game {
       monster.removeAbility(Ability.RESURRECT);
       deadMonster.resurrect();
       const deadMonsterIndex = this.deadMonsters.findIndex((deadMon) => deadMon === deadMonster);
-      deadMonster.armor = deadMonster.startingArmor;
+      deadMonster.armor = deadMonster.getPostAbilityMaxArmor();
       this.deadMonsters.splice(deadMonsterIndex, 1);
       this.createAndAddBattleLog(Ability.RESURRECT, monster, deadMonster);
       return true;
